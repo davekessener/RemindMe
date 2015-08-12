a7e289073bef3285c16c2f2bb9d2f4102ebcf17a
Dave Kessener <davekessener@gmail.com>
2015-08-12 10:44:58 +0200

    master: interface

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -1,2 +1,21 @@
 class RemindersController < ApplicationController
+  def index
+    @reminders = Reminder.all
+  end
+  
+  def create
+    @reminder = Reminder.new(reminder_params)
+    if @reminder.save
+      flash[:success] = "Successfully saved reminder for #{@reminder.send_at}!"
+      redirect_to reminders_url
+    else
+      flash[:error] = "Failed to save reminder :("
+    end
+  end
+  
+  private
+  
+    def reminder_params
+      return params.require(:reminder).permit(:recipient, :title, :message, :send_at)
+    end
 end
