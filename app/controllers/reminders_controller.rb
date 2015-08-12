4e48c0b0dd1a38c23f852a9cf8f3d8b78faceee2
Dave Kessener <davekessener@gmail.com>
2015-08-12 17:01:40 +0200

    master: fixed design

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -1,15 +1,30 @@
 class RemindersController < ApplicationController
+  def root
+    redirect_to reminders_url
+  end
+  
   def index
     @reminders = Reminder.all
   end
   
   def create
     @reminder = Reminder.new(reminder_params)
+    @reminder.was_sent = false
     if @reminder.save
-      flash[:success] = "Successfully saved reminder for #{@reminder.send_at}!"
+      flash.notice = "Successfully saved reminder for #{@reminder.send_at}!"
       redirect_to reminders_url
     else
-      flash[:error] = "Failed to save reminder :("
+      flash.alert = "Failed to save reminder :("
+    end
+  end
+  
+  def destroy
+    @reminder = Reminder.find(params[:id])
+    if @reminder.destroy
+      flash.notice = "Reminder #{@reminder.id} on #{@reminder.send_at} was deleted."
+      redirect_to reminders_url
+    else
+      flash.alert = "Remidner #{@reminder.id} coudn't be destroyed."
     end
   end
   
